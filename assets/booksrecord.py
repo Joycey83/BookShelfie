08c628e2b39d6582dedf95135765714732e46c7a
Joyce <Jcheung8385@gmail.com>
2024-03-14 15:36:33 +0000

    created the variables that I need to store the inputs for the books database

--- assets/booksrecord.py
+++ assets/booksrecord.py
@@ -5,3 +5,18 @@ from connect import *
 
 # Insert book records in the books table
 
+
+def insert_record():
+    try:
+        dbCon, dbCursor = db_access()
+
+
+        book_title = input("Enter book title:")
+        author_name = input("Enter book author:")
+        book_publisher = input("Enter publisher:")
+        book_pub_year = input("Enter book publication year:")
+        isbn_number = input("Enter ISBN Number:")
+        book_genre = input("Enter book genre:")
+        book_lang = input("Enter the language: ")
+        book_pg_count = input("Enter the page count: ")
+
