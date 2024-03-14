eb178045793fb423ec774059e4017ab481a95f98
Joyce <Jcheung8385@gmail.com>
2024-03-14 15:07:50 +0000

    created the books table

--- assets/tblsbooks.py
+++ assets/tblsbooks.py
@@ -9,4 +9,19 @@ def books_tbls():
 
     dbCon, dbCursor = db_access()
 
-    
+    dbCursor.execute(
+    """
+CREATE TABLE "books" (
+	"BookID"	INTEGER NOT NULL UNIQUE,
+	"Title"	TEXT,
+	"Author" TEXT,
+    "Publisher" TEXT,
+    "Publication_year" INTEGER,
+    "ISBN" TEXT,
+	"Genre"	TEXT,
+    "Language" TEXT,
+    "Page_count" INTEGER,
+	PRIMARY KEY("BookID" AUTOINCREMENT)
+)"""
+)
+
