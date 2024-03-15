313707081d1ed695667f07c9441f701e6f1a2946
Joyce <Jcheung8385@gmail.com>
2024-03-15 13:00:27 +0000

    amending the read record file

--- assets/readbooksrecord.py
+++ assets/readbooksrecord.py
@@ -15,13 +15,13 @@ def read_all_books():
         if all_books:
             print("*" * 300)
            #fortmat output BookID, Title, Author, Publisher etc
-            print(f"{'BookID':<6}|{'Title':<25}|{'Author':<24} | {'Publisher':<24}| {'Publication_year':<15}|{'ISBN':<15}|{'Genre':<15}|{'Language':<20}| {'Page_count':<15}|{'Price':<15}")
+            print(f"{'BookID':<6}|{'Title':<15}|{'Author':<15} | {'Publisher':<15}| {'Publication_year':<15}|{'ISBN':<15}|{'Genre':<15}|{'Language':<10}| {'Page_count':<10}|{'Price':<10}")
             print("*" * 300)
 
         
             for aBook in all_books:
                
-               print(f"{aBook[0]:<9}|{aBook[1]:<30}|{aBook[2]:<30}|{aBook[3]:<25} | {aBook[4]:<25}|{aBook[5]:<20} | {aBook[6]:<25}|{aBook[7]:<20}|{aBook[8]:<15}|{aBook[9]:<15}")
+               print(f"{aBook[0]:<9}|{aBook[1]:<30}|{aBook[2]:<10}|{aBook[3]:<10} | {aBook[4]:<10}|{aBook[5]:<10} | {aBook[6]:<10}|{aBook[7]:<10}|{aBook[8]:<10}|{aBook[9]:<10}")
                print("-" * 300)
 
 
