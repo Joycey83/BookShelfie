ea56ca4e581bff9475f29dde18b1f8559efaff17
Joyce <Jcheung8385@gmail.com>
2024-03-15 12:45:49 +0000

    trying to fix the read record file

--- assets/readbooksrecord.py
+++ assets/readbooksrecord.py
@@ -15,13 +15,13 @@ def read_all_books():
         if all_books:
             print("*" * 100)
            #fortmat output BookID, Title, Author, Publisher etc
-            print(f"{'BookID':<6} | {'Title':<25} | {'Author':<24} | {'Publisher':<24} | {'Publication_year':<15} | {'ISBN':<15} | {'Genre':<15} | {'Language':<20} | {'Page_count':<15} | {'Price':<15}")
+            print(f"{'BookID':<6}|{'Title':<25}|{'Author':<24} | {'Publisher':<24}| {'Publication_year':<15}|{'ISBN':<15}|{'Genre':<15}|{'Language':<20}| {'Page_count':<15}|{'Price':<15}")
             print("*" * 100)
 
         
             for aBook in all_books:
                
-               print(f"{aBook[0]:<9} | {aBook[1]:<30} | {aBook[2]:<30} | {aBook[3]:<25} | {aBook[4]:<25} | {aBook[5]:<20} | {aBook[6]:<25} | {aBook[7]:<20} | {aBook[8]:<15} | {aBook[9]:<15}")
+               print(f"{aBook[0]:<9}|{aBook[1]:<30}|{aBook[2]:<30}|{aBook[3]:<25} | {aBook[4]:<25}|{aBook[5]:<20} | {aBook[6]:<25}|{aBook[7]:<20}|{aBook[8]:<15}|{aBook[9]:<15}")
                print("-" * 100)
 
 
