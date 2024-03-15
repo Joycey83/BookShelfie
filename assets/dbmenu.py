e9af8b80e94a74ceec2519730231afb81fff5585
Joyce <Jcheung8385@gmail.com>
2024-03-15 16:26:04 +0000

    added the books menu function

--- assets/dbmenu.py
+++ assets/dbmenu.py
@@ -12,3 +12,11 @@ def read_file(file_path):# file_path is the parameter
             return rf
     except FileNotFoundError as not_found:
         print(f"File not found {not_found}")
+
+def books_menu():
+    option = 0 # create an integer variable call option
+    # create a list data structure hold string items/values and assign it to optionsList
+    optionsList = ["1","2","3","4","5","6"]
+ 
+    # call/invoke invoke the read_file funcion and assign it to a variable
+    menu_choices = read_file("assets/dbmenu.txt")
