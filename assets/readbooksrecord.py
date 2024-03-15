dcce691e4871c8b835250c905e8b58a304e1e5a1
Joyce <Jcheung8385@gmail.com>
2024-03-15 13:28:33 +0000

    adjust the alignement

--- assets/readbooksrecord.py
+++ assets/readbooksrecord.py
@@ -15,12 +15,12 @@ def read_all_books():
         if all_books:
             print("*" * 140)
             # format output BookID, Title, Author, Publisher etc
-            print(f"{'BookID':<9}|{'Title':<30}|{'Author':<10}|{'Publisher':<10}|{'Publication_year':<10}|{'ISBN':<10}|{'Genre':<10}|{'Language':<10}|{'Page_count':<10}|{'Price':<10}")
+            print(f"{'BookID':<5}|{'Title':<30}|{'Author':<10}|{'Publisher':<10}|{'Publication_year':<10}|{'ISBN':<10}|{'Genre':<10}|{'Language':<10}|{'Page_count':<10}|{'Price':<10}")
             print("*" * 140)
 
             for aBook in all_books:
                 aBook = [value if value is not None else '' for value in aBook]
-                print(f"{aBook[0]:<9}|{aBook[1]:<30}|{aBook[2]:<10}|{aBook[3]:<10}|{aBook[4]:<10}|{aBook[5]:<10}|{aBook[6]:<10}|{aBook[7]:<10}|{aBook[8]:<10}|{aBook[9]:<10}")
+                print(f"{aBook[0]:<5}|{aBook[1]:<30}|{aBook[2]:<10}|{aBook[3]:<10}|{aBook[4]:<10}|{aBook[5]:<10}|{aBook[6]:<10}|{aBook[7]:<10}|{aBook[8]:<10}|{aBook[9]:<10}")
                 print("-" * 140)
             
             dbCon.commit()
