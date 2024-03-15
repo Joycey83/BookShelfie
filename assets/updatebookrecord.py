9644e72bfbbf7a85ea7f0dbe54d89ab9fdd70372
Joyce <Jcheung8385@gmail.com>
2024-03-15 14:09:32 +0000

    add an if else statement to see if there is a books record available on the database

--- assets/updatebookrecord.py
+++ assets/updatebookrecord.py
@@ -8,3 +8,11 @@ def update_book_record():
 
         book_id = int(input("Enter BookID to update a record: "))
         dbCursor.execute("SELECT * FROM books WHERE BookID = ?",(book_id,))
+
+        row = dbCursor.fetchone()
+
+        if row == None:# if there is no match with the book_id provided
+            print(f"No record with BookID {book_id} exists in the books table")
+
+        else:# if there is a match with the book_id provided
+            num_fields = input("Enter N to update one field or Y to update all fields: ").upper()
