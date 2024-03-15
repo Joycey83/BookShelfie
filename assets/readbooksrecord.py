a1da864646cad4f948ff480a9e610e353fec5f8c
Joyce <Jcheung8385@gmail.com>
2024-03-15 13:03:24 +0000

    trying to sort out the f string error

--- assets/readbooksrecord.py
+++ assets/readbooksrecord.py
@@ -12,22 +12,27 @@ def read_all_books():
 
 
 
+        def read_all_books():
+    try:
+        dbCon, dbCursor = db_access()
+
+        dbCursor.execute("SELECT * FROM books")
+
+        all_books = dbCursor.fetchall()
+
         if all_books:
             print("*" * 300)
-           #fortmat output BookID, Title, Author, Publisher etc
-            print(f"{'BookID':<6}|{'Title':<15}|{'Author':<15} | {'Publisher':<15}| {'Publication_year':<15}|{'ISBN':<15}|{'Genre':<15}|{'Language':<10}| {'Page_count':<10}|{'Price':<10}")
+            # format output BookID, Title, Author, Publisher etc
+            print(f"{'BookID':<6}|{'Title':<15}|{'Author':<15}|{'Publisher':<15}|{'Publication_year':<15}|{'ISBN':<15}|{'Genre':<15}|{'Language':<10}|{'Page_count':<10}|{'Price':<10}")
             print("*" * 300)
 
-        
             for aBook in all_books:
-               
-               print(f"{aBook[0]:<9}|{aBook[1]:<30}|{aBook[2]:<10}|{aBook[3]:<10} | {aBook[4]:<10}|{aBook[5]:<10} | {aBook[6]:<10}|{aBook[7]:<10}|{aBook[8]:<10}|{aBook[9]:<10}")
-               print("-" * 300)
-
+                print(f"{aBook[0]:<9}|{aBook[1]:<30}|{aBook[2]:<10}|{aBook[3]:<10}|{aBook[4]:<10}|{aBook[5]:<10}|{aBook[6]:<10}|{aBook[7]:<10}|{aBook[8]:<10}|{aBook[9]:<10}")
+                print("-" * 300)
 
         else:
-           print("No books found in the books table")
-    except  sql.OperationalError as oe:
+            print("No books found in the books table")
+    except sql.OperationalError as oe:
         print(f"Failed to read because: {oe}")
 
 if __name__ == "__main__":
