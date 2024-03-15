181cfd7be898f432c295bcd16743e0d30814d7d6
Joyce <Jcheung8385@gmail.com>
2024-03-15 12:12:42 +0000

    added the format output f string of the table and table headings

--- assets/readbooksrecord.py
+++ assets/readbooksrecord.py
@@ -7,3 +7,11 @@ def read_all_books():
         dbCon, dbCursor = db_access()
 
         dbCursor.execute("SELECT * FROM books")
+
+        all_books = dbCursor.fetchall()
+
+        if all_books:
+           print("*" * 100)
+           #fortmat output BookID, Title, Author, Publisher etc
+           print(f"BookID{'':<3}|Title{'':<25}|Author{'':<24}|Publisher{'':24}|Publication_year{'':<15}|ISBN{'':<15}|Genre{'':<15}|Language{'':<20}|Page_count{'':<15}|Price{'':<15}")
+           print("*" * 100)
