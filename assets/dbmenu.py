db97a8c1317f92af8345fe463f85f8def2ec7024
Joyce <Jcheung8385@gmail.com>
2024-03-15 16:40:29 +0000

    add the if else statement to check if the menu options

--- assets/dbmenu.py
+++ assets/dbmenu.py
@@ -32,3 +32,29 @@ def books_menu():
         if option not in optionsList:
             print(f"{option} is not a valid choice! ")
     return option
+
+main_program = True # can be toggle to False if required(to exit the loop below)
+ 
+while main_program: #same as while True
+    # call/invoke invoke the books_menu() funcion and assign it to a variable
+ 
+    menu_options = books_menu() # books_menu() returns = "1"/"2"/"4.5..6.."
+ 
+    # check if "1"/"2"/"4.5..6.." =="1"
+    if menu_options == "1":
+        # call the file name readrecords and the function read_all_books()
+        readbooksrecord.read_all_books()
+ 
+        # check if "1"/"2"/"4.5..6.." =="2"
+    elif menu_options == "2":
+        # call the filename booksrecord and the function insert_record()
+        booksrecord.insert_record()
+    elif menu_options == "3":
+        updatebookrecord.update_book_record()
+    elif menu_options == "4":
+        deletebookrecord.delete_books()
+    elif menu_options == "5":
+        report.report()
+    else: #re-assign the value to False to exit the loop
+        main_program = False
+input("Press enter to exit: ")
