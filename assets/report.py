8dea601555a17a564bf52fb11d664aeda71550d7
Joyce <Jcheung8385@gmail.com>
2024-03-15 15:55:09 +0000

    added the other if else elif statement for when the user searches for a particular field a report is generated

--- assets/report.py
+++ assets/report.py
@@ -17,4 +17,37 @@ def report():
 
             if row is None:
                 print(f"No record with BookID {book_id} exists in the books table")
+            
+            else:
+                print("*" * 140)
+                print(f"{'BookID':<5}|{'Title':<30}|{'Author':<20}|{'Publisher':<10}|{'Publication_year':<15}|{'ISBN':<14}|{'Genre':<10}|{'Language':<10}|{'Page_count':<10}|{'Price':<5}")
+                print("*" * 140)
+                print(f"{row[0]:<5}|{row[1]:<30}|{row[2]:<20}|{row[3]:<10}|{row[4]:<15}|{row[5]:<14}|{row[6]:<10}|{row[7]:<10}|{row[8]:<10}|{row[9]:<5}")
+                print("-" * 140)
+            
+        elif search_field.title() in ["Title", "Author", "Publisher","Publication_year","ISBN","Genre","Language","page_count","Price"]:
+            #Search by Title or Artist or Genre
+            str_input = input(f"Enter the value for the field {search_field}: ")
+           
+            dbCursor.execute(f"SELECT * FROM songs WHERE {search_field} LIKE ?", (f'%{str_input}%',))
+
+            rows = dbCursor.fetchall()
+
+            if not rows:
+                print(f"No record with field {search_field} matching {str_input} in the songs table")
+            else:
+                # display all matched records from the books table
+                print("*" * 140)
+                print(f"{'BookID':<5}|{'Title':<30}|{'Author':<20}|{'Publisher':<10}|{'Publication_year':<15}|{'ISBN':<14}|{'Genre':<10}|{'Language':<10}|{'Page_count':<10}|{'Price':<5}")
+                print("*" * 140)
+                for records in rows:
+                    print(f"{records[0]:<5}|{records[1]:<30}|{records[2]:<20}|{records[3]:<10}|{records[4]:<15}|{records[5]:<14}|{records[6]:<10}|{records[7]:<10}|{records[8]:<10}|{records[9]:<5}")
+                print("-" * 140)
+                 
+        else:
+            print(f"Search field {search_field} Invalid! ")
+    except sql.OperationalError as e:
+        print(f"Search error: {e}")
+if __name__ == "__main__":
+    report()
  
