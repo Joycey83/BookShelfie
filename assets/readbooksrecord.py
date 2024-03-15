469248e7ee90c902928079353d8f0ac8dadf46d1
Joyce <Jcheung8385@gmail.com>
2024-03-15 13:59:54 +0000

    change the entire width of the columns again

--- assets/readbooksrecord.py
+++ assets/readbooksrecord.py
@@ -15,12 +15,12 @@ def read_all_books():
         if all_books:
             print("*" * 140)
             # format output BookID, Title, Author, Publisher etc
-            print(f"{'BookID':<5}|{'Title':<30}|{'Author':<20}|{'Publisher':<10}|{'Publication_year':<10}|{'ISBN':<10}|{'Genre':<10}|{'Language':<10}|{'Page_count':<10}|{'Price':<10}")
+            print(f"{'BookID':<5}|{'Title':<30}|{'Author':<20}|{'Publisher':<10}|{'Publication_year':<15}|{'ISBN':<14}|{'Genre':<10}|{'Language':<10}|{'Page_count':<10}|{'Price':<5}")
             print("*" * 140)
 
             for aBook in all_books:
                 aBook = [value if value is not None else '' for value in aBook]
-                print(f"{aBook[0]:<5}|{aBook[1]:<30}|{aBook[2]:<20}|{aBook[3]:<10}|{aBook[4]:<15}|{aBook[5]:<14}|{aBook[6]:<10}|{aBook[7]:<5}|{aBook[8]:<10}|{aBook[9]:<5}")
+                print(f"{aBook[0]:<5}|{aBook[1]:<30}|{aBook[2]:<20}|{aBook[3]:<10}|{aBook[4]:<15}|{aBook[5]:<14}|{aBook[6]:<10}|{aBook[7]:<10}|{aBook[8]:<10}|{aBook[9]:<5}")
                 print("-" * 140)
             
             dbCon.commit()
