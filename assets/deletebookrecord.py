f9044932d4ec51161deb811fac1089555f9994c7
Joyce <Jcheung8385@gmail.com>
2024-03-15 15:05:01 +0000

    fixed the indentation error issue

--- assets/deletebookrecord.py
+++ assets/deletebookrecord.py
@@ -4,7 +4,19 @@ from connect import *
 
 def delete_book_record():
     try:
-       dbCon, dbCursor = db_access()
+        dbCon, dbCursor = db_access()
        # check if songID exist
-    book_id = int(input("Enter the book ID to delete the record: "))
-    dbCursor.execute("SELECT * FROM books WHERE BookID = ?",(book_id,))
+        book_id = int(input("Enter the book ID to delete the record: "))
+        dbCursor.execute("SELECT * FROM books WHERE BookID = ?",(book_id,))
+
+        row = dbCursor.fetchone()
+
+        if row == None: # is a singleton object that checks if a value exist
+           print(f"No record with {book_id} exists!")
+        else: # if there is a match with the song id provide
+           dbCursor.execute("DELETE from books WHERE BookID = ?", (book_id,))
+           dbCon.commit()
+
+           print(f"The record with the book ID {book_id} deleted!!")
+    except  sql.OperationalError as oe:
+          print(f"Failed to read because: {oe}")
