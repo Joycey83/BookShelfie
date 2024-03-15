e32aeb23499a74afcc26d0d3d6340a9e4b2c26ec
Joyce <Jcheung8385@gmail.com>
2024-03-15 13:20:18 +0000

    adjust the format of the table

--- assets/readbooksrecord.py
+++ assets/readbooksrecord.py
@@ -13,15 +13,15 @@ def read_all_books():
 
 
         if all_books:
-            print("*" * 110)
+            print("*" * 140)
             # format output BookID, Title, Author, Publisher etc
             print(f"{'BookID':<6}|{'Title':<15}|{'Author':<15}|{'Publisher':<15}|{'Publication_year':<15}|{'ISBN':<15}|{'Genre':<15}|{'Language':<10}|{'Page_count':<10}|{'Price':<10}")
-            print("*" * 110)
+            print("*" * 140)
 
             for aBook in all_books:
                 aBook = [value if value is not None else '' for value in aBook]
                 print(f"{aBook[0]:<9}|{aBook[1]:<30}|{aBook[2]:<10}|{aBook[3]:<10}|{aBook[4]:<10}|{aBook[5]:<10}|{aBook[6]:<10}|{aBook[7]:<10}|{aBook[8]:<10}|{aBook[9]:<10}")
-                print("-" * 150)
+                print("-" * 110)
             
             dbCon.commit()
 
